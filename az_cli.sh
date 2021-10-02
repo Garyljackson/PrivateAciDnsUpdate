7e76ce869654b91322f8e1917a271cdd4627c551
Gary Jackson <garyljackson@gmail.com>
2021-10-02 10:48:15 +1000

    Fix script param

--- az_cli.sh
+++ az_cli.sh
@@ -6,6 +6,6 @@ az login --service-principal -u $APP_ID -p $APP_PASSWORD --tenant $APP_TENANT_ID
 
 ACI_IP=$(az container show --name $ACI_INSTANCE_NAME --resource-group $RESOURCE_GROUP --query ipAddress.ip --output tsv)
 
-az network private-dns record-set a update --$A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME --set aRecords[0].ipv4Address=$ACI_IP
+az network private-dns record-set a update --name $A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME --set aRecords[0].ipv4Address=$ACI_IP
 
 echo "Done"
